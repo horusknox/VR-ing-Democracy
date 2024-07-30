798660b98b3ce4fd61f0a2ba076c6c15594435bb
Shoukath Ali shaik <shoukathali271004@gmail.com>
2024-07-30 22:10:29 +0530

    Added the logo

--- src/components/global-components/navbar.js
+++ src/components/global-components/navbar.js
@@ -2,94 +2,75 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
-	
-    render() {
-        let publicUrl = process.env.PUBLIC_URL+'/'
-        let imgattr = 'logo'
-        let anchor = '#'
-        return (
-           <header className="site-header-one go-top" style={{"marginBottom":"20px"}}>
-			  <nav className="main-nav__one stricky">
-			    <div className="container-fluid">
-			      <div className="main-nav__logo-box">
-			        <Link to="/">
-			          {/* <img src={publicUrl+"assets/images/logo-full-light.png" } alt={ imgattr } /> */}
-					  <img src={publicUrl + "assets/images/logo-full-light.jpg"} alt={imgattr} style={{ transform: 'scale(4)', transformOrigin: '0 0' }} />
-			        </Link>
-					<a href="#" className="side-menu__toggler">
-      <i className="fa fa-bars"  style={{color:"black"}}/>
-    </a>			     
-	 </div>{/* /.main-nav__logo-box */}
-			      <div className="main-nav__main-navigation">
-			        <ul className=" main-nav__navigation-box">
-			          {/* <li> */}
-			          <li>
-			            <Link to="/" style={{ color: 'black' }}>Home</Link>
-			            {/* <ul>
-			              <li><Link to="/home-v2">Home 02</Link></li>
-			              <li><Link to="/home-v3">Home 03</Link></li>
-			              <li><Link to="/home-v4">Home 04</Link></li>
-			            </ul>/.sub-menu*/}
-			          </li> 
-			          <li>
-			            <Link to="/about" style={{ color: 'black' }}>About Us</Link>
-			          </li>
-			          {/* <li> */}
-			          {/* <li className="dropdown"> */}
-			            {/* <a href="#">Services</a> */}
-			            {/* <ul>
-			              <li><Link to="/service">Services 01</Link></li>
-			              <li><Link to="/service-v2">Services 02</Link></li>
-			              <li><Link to="/service-details">Services Details</Link></li>
-			            </ul> */}
-						{/* /.sub-menu */}
-			          {/* </li> */}
-			          <li>
-			          {/* <li className="dropdown"> */}
-			            {/* <a href="/QuestLocation">Quest location</a> */}
-			            <Link to="/QuestLocation" style={{ color: 'black' }}>Quest Location</Link>
-			            {/* <ul>
-			              <li><Link to="/portfolio-standard">Portfolio Standard</Link></li>
-			              <li><Link to="/portfolio-full">Portfolio Full</Link></li>
-			              <li><Link to="/portfolio-masonary">Portfolio Masonary</Link></li>
-			              <li><Link to="/portfolio-details">Portfolio Details</Link></li>
-			            </ul> */}
-						{/* /.sub-menu */}
-			          </li>
-			          {/* <li> */}
-			          {/* <li className="dropdown"> */}
-			            {/* <a href="#">Pages</a> */}
-			            {/* <ul>
-			              <li><Link to="/pricing">Pricing</Link></li>
-			              <li><Link to="/faq">FAQ</Link></li>
-			              <li><Link to="/testimonial">Testimonials</Link></li>
-			              <li><Link to="/team">Team</Link></li>
-			            </ul> */}
-						{/* /.sub-menu */}
-			          {/* </li> */}
-			          <li>
-			          {/* <li className="dropdown"> */}
-			            <Link to="/News" style={{ color: 'black' }}>News</Link>
-			            {/* <ul>
-			              <li><Link to="/blog-grid">News Page</Link></li>
-			              <li><Link to="/blog-list">News List</Link></li>
-			              <li><Link to="/blog-details">News Details</Link></li>
-			            </ul> */}
-						{/* /.sub-menu */}
-			          </li>
-			          <li>
-			            <Link to="/contact" style={{ color: 'black' }}>Contact</Link>
-			          </li>
-			        </ul>
-			      </div>{/* /.main-nav__main-navigation */}
-			      <div className="main-nav__right">
-			        {/* <Link to="/contact" className="thm-btn">FAQs<i className="fa fa-angle-right" /></Link> */}
-			      </div>
-			    </div>
-			  </nav>
-			</header>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile: window.innerWidth <= 768
+    };
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize() {
+    this.setState({ isMobile: window.innerWidth <= 768 });
+  }
+
+  render() {
+    const { isMobile } = this.state;
+    let publicUrl = process.env.PUBLIC_URL + '/';
+    let imgattr = 'logo';
+    let anchor = '#';
+
+    return (
+      <header className="site-header-one go-top" style={{ marginBottom: "20px" }}>
+        <nav className="main-nav__one stricky" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '10px 20px' }}>
+          <div className="container-fluid" style={{ position: 'relative', width: '100%', maxWidth: '1200px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div className="main-nav__logo-box" style={{ position: 'absolute', left: '10px', top: '300%', transform: 'translateY(-50%)', display: 'flex', alignItems: 'center' }}>
+              <Link to="/">
+                <img
+                  src={publicUrl + "assets/images/logo-full-light.jpg"}
+                  alt={imgattr}
+                  style={{ transform: 'scale(0.6)', transformOrigin: '0 0', display: isMobile ? 'none' : 'block' }}
+                />
+              </Link>
+              <a href="#" className="side-menu__toggler">
+                <i className="fa fa-bars" style={{ color: "black" }} />
+              </a>
+            </div>{/* /.main-nav__logo-box */}
+            <div className="main-nav__main-navigation">
+              <ul className="main-nav__navigation-box">
+                <li>
+                  <Link to="/" style={{ color: 'black' }}>Home</Link>
+                </li>
+                <li>
+                  <Link to="/about" style={{ color: 'black' }}>About Us</Link>
+                </li>
+                <li>
+                  <Link to="/QuestLocation" style={{ color: 'black' }}>Quest Location</Link>
+                </li>
+                <li>
+                  <Link to="/News" style={{ color: 'black' }}>News</Link>
+                </li>
+                <li>
+                  <Link to="/contact" style={{ color: 'black' }}>Contact</Link>
+                </li>
+              </ul>
+            </div>{/* /.main-nav__main-navigation */}
+            <div className="main-nav__right">
+              {/* <Link to="/contact" className="thm-btn">FAQs<i className="fa fa-angle-right" /></Link> */}
+            </div>
+          </div>
+        </nav>
+      </header>
+    );
+  }
 }
 
 export default Navbar;
